c61883a9112db06f4cbdf5efd9260f177b629768
Brennan Davis <brennantd@gmail.com>
2022-05-10 07:14:39 -0500

    script.js edit

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 document.getElementById("js01").addEventListener("click", function() {
-    alert(`JavaScript is the functional language for website. It makes the webpage interactive and responsive.`);});
+    alert(`JavaScript is the functional language for the internet. It makes the webpage interactive and responsive.`);});
