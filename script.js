78863b1a1300a2c20bfb4b4aaf576f06a1f3b016
Brennan Davis <brennantd@gmail.com>
2022-05-13 19:54:40 -0500

    added alerts to script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.getElementById("jsHomePageButton").addEventListener("click", function() {
-    alert(`JavaScript is the functional language for the internet. It makes the webpage interactive and responsive.`);
+    alert(`JavaScript is the functional language of the internet. It makes the webpage interactive and dynamic.`);
+});
+
+document.getElementById("htmlHomePageButton").addEventListener("click", function() {
+    alert(`HTML stands for Hyper Text Markup Language. It is the structure and bones of the website.`);
+});
+
+document.getElementById("cssHomePageButton").addEventListener("click", function() {
+    alert(`CSS stands for Cascading Styles Sheet. It is what makes this website so pretty ☺️`);
 });
